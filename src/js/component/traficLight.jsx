fa3255f168b0d5e2d54e0d736a0bb9fc0dcc7987
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-03-11 18:25:16 +0000

    light on when clicked

--- src/js/component/traficLight.jsx
+++ src/js/component/traficLight.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
 export function TraficLight() {
+	const [color, setColor] = useState("yellow");
 	return (
 		<div className="app">
 			<div className="tlTop"></div>
-			<div className="tlBody">
-				<div className="red light selected"></div>
-				<div className="yellow light"></div>
-				<div className="green light"></div>
-			</div>
+				<div className="tlBody">
+					<div
+						className={
+							color === "red" ? "red light selected" : "red light"
+						}
+						onClick={() => setColor("red")}></div>
+					<div
+						className={
+							color === "yellow"
+								? "yellow light selected"
+								: "yellow light"
+						}
+						onClick={() => setColor("yellow")}></div>
+					<div
+						className={
+							color === "green"
+								? "green light selected"
+								: "green light"
+						}
+						onClick={() => setColor("green")}></div>
+				</div>
 		</div>
 	);
 }
